9fd208bf35e9029c28b69ab576ba1fba9b5defe2
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-06-25 19:15:08 +0700

    tutorial 21

--- src/sever.js
+++ src/sever.js
@@ -13,6 +13,12 @@ const hostname = process.env.HOST_NAME || "localhost";
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//config static files
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+
 // khai báo route
 app.get("/", (req, res) => {
   res.send("Hello World! vs Bui Tan Phat learn backend");
@@ -23,6 +29,8 @@ app.get("/abc", (req, res) => {
 app.get("/buitanphat", (req, res) => {
   res.render("sample.ejs");
 });
+
+
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
