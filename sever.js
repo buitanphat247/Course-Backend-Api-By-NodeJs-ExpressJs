ee8414e534d3b1a68558d6d9e2bf1585016927ec
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-06-25 17:56:34 +0700

    tutorial 14

--- sever.js
+++ sever.js
@@ -1,14 +1,11 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const hostname = "localhost"; // is localhost
-const port = 3000; // is port 3000 nên đặt port là 8000 || 8001,...
-
-const sever = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("hello world\n Hoc backend cung bui tan phat");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-sever.listen(port, hostname, () => {
-  console.log(`sever running at http://${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
