3ade3938cd3735846b3dc1561d74019110e42c95
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-06-25 18:38:01 +0700

    tutorial 18

--- src/sever.js
+++ src/sever.js
@@ -1,17 +1,22 @@
-const express = require("express");// common js
+const express = require("express"); // common js
+const path = require("path");
 // import express from "express // esmodules
 const app = express(); // app express
 const port = 3000; // port
 
+// config template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 // khai báo route
-app.get("/", (req, res) => { 
+app.get("/", (req, res) => {
   res.send("Hello World! vs Bui Tan Phat learn backend");
 });
-app.get("/abc", (req, res) => { 
+app.get("/abc", (req, res) => {
   res.send("Hello World! vs Bui Tan Phat learn backend in route abc");
 });
-app.get("/buitanphat", (req, res) => { 
-  res.send("<h1>bùi tấn phát</h1>");
+app.get("/buitanphat", (req, res) => {
+  res.render("sample.ejs");
 });
 
 app.listen(port, () => {
