923e58a2f2ab08a510aa231fb28c4afd177d5ab7
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-06-25 18:19:54 +0700

    tutorial 17

--- sever.js
+++ sever.js
@@ -7,6 +7,12 @@ const port = 3000; // port
 app.get("/", (req, res) => { 
   res.send("Hello World! vs Bui Tan Phat learn backend");
 });
+app.get("/abc", (req, res) => { 
+  res.send("Hello World! vs Bui Tan Phat learn backend in route abc");
+});
+app.get("/buitanphat", (req, res) => { 
+  res.send("<h1>bùi tấn phát</h1>");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
