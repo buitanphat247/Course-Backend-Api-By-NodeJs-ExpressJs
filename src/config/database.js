ccabb260af0024ad91f77f404cb92618a1175df4
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-06-26 09:04:32 +0700

    tutorial 31

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+// test connection
+// create connection to sever
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+module.exports = connection;
--- src/sever.js
+++ src/sever.js
@@ -1,22 +1,15 @@
 require("dotenv").config();
 const express = require("express"); // common js
-const mysql = require("mysql2");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const connection = require("./config/database");
 const app = express(); // app express
 const port = process.env.PORT || 8888; // port
 const hostname = process.env.HOST_NAME || "localhost";
 
 configViewEngine(app);
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-
+// test connection query
 connection.query("select * from Users ", function (err, results, fields) {
   console.log(results);
 });
