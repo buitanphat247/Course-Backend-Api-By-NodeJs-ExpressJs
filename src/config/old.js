f75719356ed37733b37b935f8613e07d04de5169
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-06-26 09:51:16 +0700

    tutorial 33

---
+++ src/config/old.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql2");
+
+// test connection
+// create connection to sever
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Create the connection pool. The pool-specific settings are the defaults
+// const connection = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+module.exports = connection;
