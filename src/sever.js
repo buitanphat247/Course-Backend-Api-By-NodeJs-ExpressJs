39ad179de7b34c5fdde0df180d82fcb94e4d830b
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-06-26 08:55:44 +0700

    tutorial 30

--- src/sever.js
+++ src/sever.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express"); // common js
+const mysql = require("mysql2");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const app = express(); // app express
@@ -8,6 +9,17 @@ const hostname = process.env.HOST_NAME || "localhost";
 
 configViewEngine(app);
 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+connection.query("select * from Users ", function (err, results, fields) {
+  console.log(results);
+});
 // khai báo route
 app.use("/", webRoutes);
 
