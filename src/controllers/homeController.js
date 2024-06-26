73890f6e3c39db7b3ae74e285d29b414463a0b5b
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-06-26 10:17:14 +0700

    tutorial 35

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,11 +1,7 @@
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-  let users = [];
-  connection.query("select * from Users u", function (err, results, fields) {
-    users = results;
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home.ejs");
 };
 const getAbc = (req, res) => {
   res.send("Hello World! vs Bui Tan Phat learn backend in route abc");
